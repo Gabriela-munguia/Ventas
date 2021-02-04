2aab0cbfe1eec20c876a13026c0874f25dc1f797
Gabriela Munguia <cursoscomputacion.net@gmail.com>
2021-02-04 10:56:10 -0600

    frase motivadora

--- Ventas/Ventas/Form1.Designer.cs
+++ Ventas/Ventas/Form1.Designer.cs
@@ -35,7 +35,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(82, 93);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 38);
             this.button1.TabIndex = 0;
             this.button1.Text = "procesar";
             this.button1.UseVisualStyleBackColor = true;
--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola mundo");
+            MessageBox.Show("El exito es la suma de pequeños esfuerzos repetidos dia tras dias.");
         }
     }
 }
